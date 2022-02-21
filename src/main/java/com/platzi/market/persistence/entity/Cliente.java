c07335ff5f925fd45e1fcc3dc375924b95eae1cc
miguelhmp <miguelhmp@gmail.com>
2022-02-21 12:33:13 -0700

    Project Structure - Creating Entities

---
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,29 @@
+package com.platzi.market.persistence.entity;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private String id;
+
+  private String nombre;
+
+  private String apellidos;
+
+  private Integer celular;
+
+  private String direccion;
+
+  private String correoElectronico;
+
+  @OneToMany(mappedBy = "cliente")
+  private List<Compra> compras;
+
+}
